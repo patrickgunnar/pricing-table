c87b2a2895bcd13f38812d7cb98a3a4a09ce2156
Patrick Gunnar <patrickgunnar.me@gmail.com>
2025-01-02 12:36:50 -0300

    feat: added actions.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import GaladrielWebpackClient from "@galadrielcss/webpack";
 
 const nextConfig: NextConfig = {
+    output: "export",
+    basePath: "/pricing-table",
+    trailingSlash: true,
     /* config options here */
     webpack(config) {
         config.plugins.push(new GaladrielWebpackClient());
