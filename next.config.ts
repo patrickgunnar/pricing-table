73322b075ea4a4eb89ae1286878a78cce2dda5c3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2025-01-02 12:49:48 -0300

    feat: removed workflows.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 import GaladrielWebpackClient from "@galadrielcss/webpack";
 
 const nextConfig: NextConfig = {
-    output: "export",
-    basePath: "/pricing-table",
-    trailingSlash: true,
     /* config options here */
     webpack(config) {
         config.plugins.push(new GaladrielWebpackClient());
